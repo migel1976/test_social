d0296b0bcb99ef12dd4922062d6defd8b385132b
misha <migel1976@gmail.com>
2020-04-15 13:48:44 +0300

    first commit

---
+++ src/components/app/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../header/header';
+
+const App =()=>{
+	return(
+		<div>
+			<Header />
+			Aloha
+		</div>
+	)
+};
+export default App;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './header.module.css';
+
+const Header=()=>{
+	return(
+		<div className={style.header}>
+			<h1>Заголовок</h1>
+		</div>
+	)
+};
+export default Header;
